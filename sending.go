7e7eeda2d950264101d07b75c038884f9ad219e5
Anna <anna.vasileva@green-api.com>
2025-11-14 10:26:56 +0300

    upd validation

--- sending.go
+++ sending.go
@@ -636,6 +636,7 @@ func (c SendingCategory) ForwardMessages(chatId, chatIdFrom string, messages []s
 
 type InteractiveButton struct {
 	Type        string `json:"type"`
+	Type        string `json:"type"`
 	ButtonId    string `json:"buttonId"`
 	ButtonText  string `json:"buttonText"`
 	CopyCode    string `json:"copyCode,omitempty"`
