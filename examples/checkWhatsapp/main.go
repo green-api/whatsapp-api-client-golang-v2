7497cbed707e80f465a2deabc5b361e28e76e29f
Vadim Savilov <vadim.savilov@green-api.com>
2026-07-07 18:21:23 +0500

    Removed the obsolete methods that I had added myself in the current commit.

--- examples/checkWhatsapp/main.go
+++ examples/checkWhatsapp/main.go
@@ -15,7 +15,7 @@ func main() {
 		APITokenInstance: "d75b3a66374942c5b3c019c698abc2067e151558acbd412345",
 	}
 
-	response, err := GreenAPI.Service().CheckWhatsapp(0, greenapi.OptionalChatID("77057538541@c.us"))
+	response, err := GreenAPI.Service().CheckWhatsapp(0, greenapi.OptionalChatID("11001211111@c.us"))
 	if err != nil {
 		log.Fatal(err)
 	}
